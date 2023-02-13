c8de1999dd80c1aa266fa8e456d585e734dd7161
Pedro Reyna III <p.reyna3@gmail.com>
2023-02-13 15:39:10 -0600

    added router

---
+++ src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 class="not-found">404, page not found</h2>
+</template>
+
+<style scoped lang="postcss">
+.not-found {
+  @apply pt-80 text-center text-7xl font-bold;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '@/views/HomePage.vue'
+import MainPage from '@/components/MainPage.vue'
+import LoginPage from '@/components/LoginPage.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/', name: 'Home', component: MainPage },
+  { path: '/login', name: 'Login', component: LoginPage },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 
 const router = createRouter({
