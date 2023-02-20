cdd561836afcb01015b92af2a090c26fb5ce8bdc
Pedro Reyna III <p.reyna3@gmail.com>
2023-02-20 17:47:07 -0600

    added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@ import { useAuth } from '@/composables/useAuth'
 
 const { isAuthenticated, logout, user } = useAuth()
 
-const brand = ref('🦈 MegaByte Computer Company Directory')
+const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
