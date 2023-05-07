df061c6ef0591431016b68b8ab824a47e98452d4
Pedro Reyna III <p.reyna3@gmail.com>
2023-05-07 09:53:11 -0500

    Main page get employees

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,21 @@
 <script setup>
+import { onMounted } from 'vue'
+
 import MainSearch from '@/components/MainSearch.vue'
 import MainCards from '@/components/MainCards.vue'
 import MainPagination from '@/components/MainPagination.vue'
+
+import useAPI from '@/composables/useAPI'
+
+const { getEmployees, loading } = useAPI()
+onMounted(async () => {
+  await getEmployees()
+})
 </script>
 
 <template>
-  <div class="wrapper">
+  <p v-if="loading">Loading...</p>
+  <div v-else class="wrapper">
     <MainSearch />
     <MainCards />
     <MainPagination v-show="true" style="position: fixed; bottom: 0; right: 0; width: 100%" md-mode="indeterminate" />
